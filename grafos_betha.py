8ba65567c3a1d8c90de3837509ce0b34d722e9d0
robot-beep <52836102+robot-beep@users.noreply.github.com>
2020-12-06 20:03:59 -0300

    Add files via upload
    se terminan los algoritmos, se cambian los colores del grafico, falta la parte del aaron

--- grafos_betha.py
+++ grafos_betha.py
@@ -16,6 +16,8 @@ except:#Si no se logran importar, se procede a instalarlas en el sistema del usu
     print("Libraries were installed to execute the program\n")
     sys.exit("Please restart the program") 
 
+#variables globales.
+
 def rellenarMatrizDeAdyacencia(matriz, contenedor):
     anterior = 0
     tiempoVentanas = 60
@@ -25,40 +27,47 @@ def rellenarMatrizDeAdyacencia(matriz, contenedor):
         if(a % 2 == 0):
             matriz[int(anterior)][int(aux[2])] += 1 
             
-        #if( round(float(aux[3])) >= tiempoVentanas and a % 2 != 0):
-         #   print(f'Tiempo conversacion real:{round(float(aux[3]))} || Tiempo a comparar: {tiempoVentanas}')
-          #  tiempoVentanas += 60
-           # graficarGrafo(matriz)
+        if( round(float(aux[3])) >= tiempoVentanas and a % 2 != 0):
+            print(f'Tiempo conversacion real:{round(float(aux[3]))} || Tiempo a comparar: {tiempoVentanas}')
+            tiempoVentanas += 60
+            matriz_timestamp.append(np.copy(matriz))
             
         anterior = int(aux[2])
-
+    matriz_timestamp.append(np.copy(matriz))
     graficarGrafo(matriz)
     return matriz
 
 def graficarGrafo(matriz):
     for x in range(0 , len(matriz[0])):
         for y in range(0, len(matriz[0])):
-           G.add_edge(x, y, weight = (matriz[x, y])* 0.03, length = 0.1)
+           G.add_edge(x, y, weight = (matriz[x, y])* 0.01, length = 0.1)
 
     print(f'{matriz} \n')       
     weights = nx.get_edge_attributes(G,'weight').values()
     pos = nx.circular_layout(G)
-    nx.draw(G, pos,with_labels=True,node_size=3400,font_size = 20, node_color = 'orange',width=list(weights), connectionstyle='arc3, rad = 0.1')
+    color_map = ["green", "blue", "red", "orange", "purple"]
+    nx.draw(G, pos, with_labels=True, node_size=3400, font_size=20,
+    node_color=color_map, width=list(weights), connectionstyle='arc3, rad = 0.13')
     plt.show(block=False)
     plt.pause(5)
     plt.close()
 
-def prompting(matriz):
-    suma_w_nodos = []
-    aux = 0
-    w_total = 0
-    for i in range(len(matriz)):
-        for j in range(len(matriz[0])):
-            aux += matriz[i][j]
-        suma_w_nodos.append(aux)
-        w_total += aux
-        aux = 0
-    return suma_w_nodos, w_total   
+def prompting(contenedor):
+    f = 0
+    for i in range(1,len(contenedor)):
+        aux = (contenedor[i].replace('"', "")).split(",")
+        vector_contador[int(aux[2])] += 1 
+        n = aux[3]
+        a = n.strip("/n")
+        a = int(float(a))
+        if a%60 == 0 and f != a/60:
+            matriz_contador_time.append(np.copy(vector_contador))
+            f = a/60        
+    print(vector_contador)
+    print(matriz_contador_time)
+
+    
+
 
 def main():
     fileName = 'Grupo1-393371.csv'
@@ -71,8 +80,9 @@ def main():
 
     matriz= np.zeros((5, 5))
     matriz_total = rellenarMatrizDeAdyacencia(matriz, contenedor)
-    print(matriz_total)
-    print(prompting(matriz_total))
+    prompting(contenedor)
+
+
 
 #CREAMOS LA ESTRUCTURA DEL GRAFO CON SUS NODOS
 G = nx.DiGraph()
@@ -81,5 +91,9 @@ G.add_node(1,pos=(4,2))
 G.add_node(2,pos=(1,4))
 G.add_node(3,pos=(0,2))
 G.add_node(4,pos=(2,0))
+matriz_timestamp = [] #guarda el peso de los vectores a través del tiempo 
+vector_contador = [0,0,0,0,0] #lista que contiene el peso de los vectoes al final de toda la comunicación 
+matriz_contador_time = [] #lista que contiene vectores con el peso de cada nodo a través del tiempo
+
 #---------------------------------------------
 main()
